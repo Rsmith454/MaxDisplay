7434cd635ceb0dc2b756715a6a43e444d3ad21a2
Roland Kováč <r.kovacs.smith@gmail.com>
2024-09-15 13:38:06 +0200

    adding new files

---
+++ src/lv_speedometer/lv_ui.h
@@ -0,0 +1,49 @@
+#ifndef LV_DEMO_WIDGETS_H
+#define LV_DEMO_WIDGETS_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+    /*********************
+     *      INCLUDES
+     *********************/
+#include <lvgl.h>
+
+     /*********************
+      *      DEFINES
+      *********************/
+
+      /**********************
+       *      TYPEDEFS
+       **********************/
+
+       /**********************
+        * GLOBAL PROTOTYPES
+        **********************/
+    void lv_create_main_screen(void);
+
+    /**********************
+     *      MACROS
+     **********************/
+    LV_FONT_DECLARE(Michroma_125);
+    LV_FONT_DECLARE(Michroma_110);
+    LV_FONT_DECLARE(Michroma_102);
+    LV_FONT_DECLARE(RedditSans_125);
+    LV_FONT_DECLARE(RedditSans_60);
+    LV_FONT_DECLARE(RedditSans_32);
+    LV_FONT_DECLARE(BebasNeue_125);
+    LV_FONT_DECLARE(BebasNeue_95);
+    LV_FONT_DECLARE(Michroma_90);
+    LV_FONT_DECLARE(Michroma_82);
+    LV_FONT_DECLARE(Michroma_72);
+    LV_FONT_DECLARE(Michroma_50);
+    LV_FONT_DECLARE(Michroma_46);
+    LV_FONT_DECLARE(Michroma_42);
+    LV_FONT_DECLARE(Michroma_22);
+
+#ifdef __cplusplus
+    } /* extern "C" */
+#endif
+
+#endif /*LV_DEMO_WIDGETS_H*/
